bd5be53ba1391f28e6bf13904e72a7c88eac072b
Andrey Borysenko <andrey18106x@gmail.com>
2023-10-25 20:06:21 +0300

    maintenance update, support of Nextcloud 28
    Signed-off-by: Andrey Borysenko <andrey18106x@gmail.com>

--- src/components/SentenceMark.vue
+++ src/components/SentenceMark.vue
@@ -1,47 +1,50 @@
 <template>
-	<div v-tooltip="perplexityText" class="sentence-mark">
-		<p class="sentence-mark__text"
-			:class="{'sentence-mark__text_generated': sentence.generated_prob === 1}">
-			{{ sentence.sentence }}
-		</p>
-	</div>
+  <div v-tooltip="perplexityText" class="sentence-mark">
+    <p class="sentence-mark__text"
+      :class="{'sentence-mark__text_generated': likelyGenerated}">
+      {{ sentence.sentence }}
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'SentenceMark',
-	props: {
-		sentence: {
-			type: Object,
-			required: true,
-		},
-	},
-	data() {
-		return {
-		}
-	},
-	computed: {
-		perplexityText() {
-			return t('integration_gptzero', 'Perplexity: {perplexity}', { perplexity: this.sentence.perplexity })
-		},
-	},
-	methods: {
-	},
+  name: 'SentenceMark',
+  props: {
+    sentence: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    perplexityText() {
+      return t('integration_gptzero', 'Perplexity: {perplexity}', { perplexity: this.sentence.perplexity })
+    },
+    likelyGenerated() {
+      return this.sentence.generated_prob === 1 || this.sentence.generated_prob >= 0.9 || this.sentence?.highlight_for_ai
+    },
+  },
+  methods: {
+  },
 }
 </script>
 
 <style lang="scss" scoped>
 .sentence-mark {
-	margin-bottom: 10px;
+  margin-bottom: 10px;
 
-	&__text {
-		display: inline;
-		line-height: 1.25rem;
+  &__text {
+    display: inline;
+    line-height: 1.25rem;
 
-		&_generated {
-			margin-bottom: 5px;
-			background-color: rgba(var(--color-warning-rgb), 0.2);
-		}
-	}
+    &_generated {
+      margin-bottom: 5px;
+      background-color: rgba(var(--color-warning-rgb), 0.2);
+    }
+  }
 }
 </style>
